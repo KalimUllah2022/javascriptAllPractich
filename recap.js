125959b4c9b200c90c52c9d898bbed8cf3b6377c
KalimUllah2022 <villageprogrammer2022@gmail.com>
2022-01-31 18:56:08 +0600

    commit

--- recap.js
+++ recap.js
@@ -1,4 +1,4 @@
-// variable part
+// Variable part
 // var a = 'water';
 // console.log(a);
 
@@ -6,6 +6,58 @@
 // console.log(myName);
 
 
-// data type
+// Data type
 // Primitive data type
-// string data type
+// 1.String data type
+// var firstName = 'Kalim';
+// var lastName = 'Ullah';
+// var fullName = firstName + ' ' + lastName; // concatenation
+// console.log(fullName);
+
+// 2. Number data type
+// var age1 = 30;
+// var age2 = '40'
+// var sum = age1 + parseInt(age2);
+// console.log(sum);
+
+
+// 3. Boolean data type
+// var number1 = 60;
+// var number2 = 60;
+// var addAll = number1 == number2;
+// console.log(addAll);
+
+
+// 4. Undefined data type
+// var rollNumber;
+// console.log(typeof rollNumber);
+
+
+// 5. Null data type
+// var x = null;
+// console.log(x);
+
+
+// Conditionals
+// var numberOne = 30;
+// var numberTwo = 40;
+// console.log(numberOne > numberTwo);
+// if(numberOne < numberTwo){
+//     console.log('Number Two is big');
+// }
+
+// var number = 50;
+// if(number % 2 == 0){
+//     console.log('The number is Even/Jor');
+// }
+// if(number % 2 == 1){
+//     console.log('The number is Odd/Bijor');
+// }
+
+
+
+
+
+
+
+
